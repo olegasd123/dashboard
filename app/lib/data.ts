baa259e214c8a6a6abda0f750624d3fc25a1d64e
Oleg <oleg.verhoglyad@gmail.com>
2024-07-26 19:48:01 +0300

    Overview data loading.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,12 +14,17 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    console.log('fetchRevenue.started');
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('fetchRevenue.ended');
+
+    console.log('Data fetch completed after 3 seconds.');
+
 
     return data.rows;
   } catch (error) {
@@ -30,12 +35,15 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    console.log('fetchLatestInvoices.started');
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+      console.log('fetchLatestInvoices.ended');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -53,12 +61,16 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+
+    console.log('fetchCardData.started');
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
+
+    console.log('fetchCardData.ended');
 
     const data = await Promise.all([
       invoiceCountPromise,
